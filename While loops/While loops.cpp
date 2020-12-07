0bd54c5b969642447cf591b5fdcd7d9a55efd06e
Kara Määttä <Karadiina.ma@gmail.com>
2020-12-07 16:05:31 +0200

    updated and cleaned whole damn code :,D

--- While loops/While loops.cpp
+++ While loops/While loops.cpp
@@ -1,18 +1,31 @@
 
 #include <iostream>
 
-int main()
-{
+void show_menu() {
+	std::cout << "1. Adittion. " << std::endl;
+	std::cout << "2. Subtraction. " << std::endl;
+	std::cout << "3. Multiplication. " << std::endl;
+	std::cout << "4. Division " << std::endl;
+	std::cout << "5. Quit program. " << std::endl;
+}
+int process_selection() {
+		int selection;
+	std::cout << "Enter your selection > " << std::flush;
+	std::cin >> selection;
+	return selection;
+	
+	
 
-	while (true) {
+}
 
-		std::cout << "1. Adittion. " << std::endl;
-		std::cout << "2. Subtraction. " << std::endl;
-		std::cout << "3. Multiplication. " << std::endl;
-		std::cout << "4. Division " << std::endl;
-		std::cout << "5. Quit program. " << std::endl;
 
-		std::cout << "Enter your selection > " << std::flush;
+int main(){
+	bool i = true;
+	while (i == true) {
+
+
+		show_menu();
+		int selection = process_selection();
 
 		std::string Question1;
 		Question1 = "Give first number > ";
@@ -21,14 +34,12 @@ int main()
 		std::string Awnser;
 		Awnser = "The awnser is. ";
 
-		int selection;
 		int value1;
 		int value2;
-		std::cin >> selection;
 
 
-
-		if (selection == 1) {
+		switch (selection) {
+		case 1: {
 			std::cout << Question1 << std::flush;
 			std::cin >> value1;
 			std::cout << Question2 << std::flush;
@@ -37,9 +48,9 @@ int main()
 
 			std::cout << Awnser << value1 + value2 << std::endl;
 			std::cout << "\r" << std::endl;
-
+			break;
 		}
-		else if (selection == 2) {
+		case 2: {
 			std::cout << Question1 << std::flush;
 			std::cin >> value1;
 			std::cout << Question2 << std::flush;
@@ -48,8 +59,9 @@ int main()
 
 			std::cout << Awnser << value1 - value2 << std::endl;
 			std::cout << "\r" << std::endl;
+			break;
 		}
-		else if (selection == 3) {
+		case 3: {
 			std::cout << Question1 << std::flush;
 			std::cin >> value1;
 			std::cout << Question2 << std::flush;
@@ -58,8 +70,9 @@ int main()
 
 			std::cout << Awnser << value1 * value2 << std::endl;
 			std::cout << "\r" << std::endl;
+			break;
 		}
-		else if (selection == 4) {
+		case 4: {
 			std::cout << Question1 << std::flush;
 			std::cin >> value1;
 			std::cout << Question2 << std::flush;
@@ -68,19 +81,20 @@ int main()
 
 			std::cout << Awnser << value1 / value2 << std::endl;
 			std::cout << "\r" << std::endl;
-		}
-		else if (selection == 5) {
-			std::cout << "\r" << std::endl;
-			std::cout << "Quiting .... " << std::endl;
 			break;
 		}
-		else {
+		case 5: {
+			std::cout << "\r" << std::endl;
+			std::cout << "Quiting .... " << std::endl;
+			i = false;
+			break;
+		}
+		default: {
 			std::cout << "Invalid otion..." << std::endl;
 			std::cout << "\r" << std::endl;
 		}
-
+		}
 	}
-    
-    
+	}
 
-}
+	
