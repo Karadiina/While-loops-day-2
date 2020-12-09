58e16c49b3b5602f85e33275a8d06383ff27f170
Kara Määttä <Karadiina.ma@gmail.com>
2020-12-09 22:05:12 +0200

    broken

--- While loops/While loops.cpp
+++ While loops/While loops.cpp
@@ -13,87 +13,88 @@ int get_input() {
 	std::cout << "Enter your selection > " << std::flush;
 	std::cin >> selection;
 	return selection;
-	
-	
+}
+int Proces_input() {
+	int selection = get_input();
+	std::string Question1;
+	Question1 = "Give first number > ";
+	std::string Question2;
+	Question2 = "Give second number > ";
+	std::string Awnser;
+	Awnser = "The awnser is. ";
+
+	int value1;
+	int value2;
+
+
+	switch (selection) {
+	case 1: {
+		std::cout << Question1 << std::flush;
+		std::cin >> value1;
+		std::cout << Question2 << std::flush;
+		std::cin >> value2;
+		std::cout << "\r" << std::endl;
+
+		std::cout << Awnser << value1 + value2 << std::endl;
+		std::cout << "\r" << std::endl;
+		break;
+	}
+	case 2: {
+		std::cout << Question1 << std::flush;
+		std::cin >> value1;
+		std::cout << Question2 << std::flush;
+		std::cin >> value2;
+		std::cout << "\r" << std::endl;
+
+		std::cout << Awnser << value1 - value2 << std::endl;
+		std::cout << "\r" << std::endl;
+		break;
+	}
+	case 3: {
+		std::cout << Question1 << std::flush;
+		std::cin >> value1;
+		std::cout << Question2 << std::flush;
+		std::cin >> value2;
+		std::cout << "\r" << std::endl;
+
+		std::cout << Awnser << value1 * value2 << std::endl;
+		std::cout << "\r" << std::endl;
+		break;
+	}
+	case 4: {
+		std::cout << Question1 << std::flush;
+		std::cin >> value1;
+		std::cout << Question2 << std::flush;
+		std::cin >> value2;
+		std::cout << "\r" << std::endl;
+
+		std::cout << Awnser << value1 / value2 << std::endl;
+		std::cout << "\r" << std::endl;
+		break;
+	}
+	case 5: {
+		std::cout << "\r" << std::endl;
+		std::cout << "Quiting .... " << std::endl;
+		return 2;
+		break;
+	}
+	default: {
+		std::cout << "Invalid otion..." << std::endl;
+		std::cout << "\r" << std::endl;
+	}
+	}
 
 }
 
 
 int main(){
-	bool i = true;
-	while (i == true) {
-
-
+	int i = 1;
+	while (i < 2) {
+		
 		show_menu();
-		int selection = get_input();
-
-		std::string Question1;
-		Question1 = "Give first number > ";
-		std::string Question2;
-		Question2 = "Give second number > ";
-		std::string Awnser;
-		Awnser = "The awnser is. ";
-
-		int value1;
-		int value2;
-
-
-		switch (selection) {
-		case 1: {
-			std::cout << Question1 << std::flush;
-			std::cin >> value1;
-			std::cout << Question2 << std::flush;
-			std::cin >> value2;
-			std::cout << "\r" << std::endl;
-
-			std::cout << Awnser << value1 + value2 << std::endl;
-			std::cout << "\r" << std::endl;
-			break;
-		}
-		case 2: {
-			std::cout << Question1 << std::flush;
-			std::cin >> value1;
-			std::cout << Question2 << std::flush;
-			std::cin >> value2;
-			std::cout << "\r" << std::endl;
-
-			std::cout << Awnser << value1 - value2 << std::endl;
-			std::cout << "\r" << std::endl;
-			break;
-		}
-		case 3: {
-			std::cout << Question1 << std::flush;
-			std::cin >> value1;
-			std::cout << Question2 << std::flush;
-			std::cin >> value2;
-			std::cout << "\r" << std::endl;
-
-			std::cout << Awnser << value1 * value2 << std::endl;
-			std::cout << "\r" << std::endl;
-			break;
-		}
-		case 4: {
-			std::cout << Question1 << std::flush;
-			std::cin >> value1;
-			std::cout << Question2 << std::flush;
-			std::cin >> value2;
-			std::cout << "\r" << std::endl;
-
-			std::cout << Awnser << value1 / value2 << std::endl;
-			std::cout << "\r" << std::endl;
-			break;
-		}
-		case 5: {
-			std::cout << "\r" << std::endl;
-			std::cout << "Quiting .... " << std::endl;
-			i = false;
-			break;
-		}
-		default: {
-			std::cout << "Invalid otion..." << std::endl;
-			std::cout << "\r" << std::endl;
-		}
-		}
+		Proces_input();
+		int i = Proces_input();
+		
 	}
 	}
 
